719f67ede847cd1d65060149dd607b5589c9906e
witoldzol <blue3656@gmail.com>
2018-02-02 19:14:32 +0000

    learning callbacks

--- callbacks_tut.js
+++ callbacks_tut.js
@@ -1,14 +1,29 @@
 let cl = (x)=>{console.log(x)};
 
 
-let callback = ()=>{
+let cb = ()=>{
 
     cl('2');
+    cb2();
+}
+
+
+let cb2 = ()=>{
+    cl('3');
 }
 
 let count = (x,callback)=>{
-    cl(x);
-    callback;
-]
 
-count('1',callback());
+    cl(x);
+    let y = 1 + x;
+    if(typeof callback === 'function'){
+	callback( ()=>{
+
+	    cb2();
+
+	} );
+    }
+    
+}
+
+count('1', cb );
